d93dc86782193d501ab860297a60fbac7af5ba57
LocNg <lnus8889@gmail.com>
2023-11-22 01:38:35 -0600

    adding context

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,5 +10,6 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'MyFirstProject';
+  title = 'Loc Nguyen';
+  message = 'this is the first message and I am testing'
 }
